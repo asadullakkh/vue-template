9cf5d3682012e27ddea3d17d09c34a75cd5246d9
Asadbek <a.eshboev@uic.group>
2022-03-29 04:55:06 +0500

    feat: saving files into github

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello world</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('hello')
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/helpers/global.js
@@ -0,0 +1,10 @@
+/**
+ * Use this file to register any variables or functions that should be available globally
+ * ideally you should make it available via the window object
+ * as well as the Vue prototype for access throughout the app
+ * (register globals with care, only when it makes since to be accessible app wide)
+ */
+const app = createApp({});
+app.config.globalProperties.$csl = (val) => {
+    console.log(val);
+};
