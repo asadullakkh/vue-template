3b5c0e409dffdbaf80a97add7a08e03112de42b4
Asadbek <a.eshboev@uic.group>
2022-04-12 20:56:31 +0500

    refactor: deleted unnessery files

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import router from './router/index';
 import axios from './plugins/axios';
 import VueAxios from 'vue-axios';
 import store from './store';
-
 import {
 	registerGlobalProperties,
 	hideElement,
@@ -15,7 +14,6 @@ import {
 } from './helpers/global';
 
 registerGlobalProperties.call(app, { hideElement, addParams });
-
 app.use(router);
 app.use(i18n);
 app.use(VueAxios, axios);
